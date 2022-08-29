b4288c2a741c9b4feac6eb097791e5c66cf0c9b9
Md Sakibul Hasan <sakibal623@gmail.com>
2022-08-29 22:43:36 +0600

    Token approval.

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,11 @@
+/* eslint-disable jest/valid-expect */
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const tokens = (n) => ethers.utils.parseUnits(n.toString(), "ether");
 
 describe("Token", () => {
-  let token, account, deployer, receiver;
+  let token, account, deployer, receiver, exchange;
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token");
@@ -13,6 +14,7 @@ describe("Token", () => {
     account = await ethers.getSigners();
     deployer = account[0];
     receiver = account[1];
+    exchange = account[2];
   });
 
   describe("Deployment", () => {
@@ -87,6 +89,46 @@ describe("Token", () => {
           token
             .connect(deployer)
             .transfer("0x0000000000000000000000000000000000000000", amount)
+        ).reverted;
+      });
+    });
+  });
+
+  describe("Approving Tokens", () => {
+    let amount, transaction, result;
+
+    beforeEach(async () => {
+      amount = tokens(100);
+      transaction = await token
+        .connect(deployer)
+        .approve(exchange.address, amount);
+      result = await transaction.wait();
+    });
+
+    describe("Success", () => {
+      it("allocates an allowance for delefated token spending", async () => {
+        expect(await token.allowance(deployer.address, exchange.address)).equal(
+          amount
+        );
+      });
+
+      it("emits an Approval event", async () => {
+        const event = result.events[0];
+        expect(event.event).equal("Approval");
+
+        const args = event.args;
+        expect(args.owner).equal(deployer.address);
+        expect(args.spender).equal(exchange.address);
+        expect(args.value).equal(amount);
+      });
+    });
+
+    describe("Failure", () => {
+      it("rejects invalid spenders", async () => {
+        await expect(
+          token
+            .connect(deployer)
+            .approve("0x0000000000000000000000000000000000000000", amount)
         ).reverted;
       });
     });
