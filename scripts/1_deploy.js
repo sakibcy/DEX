419472be4c7158881ae8cb57e6a7cae842f02733
Md Sakibul Hasan <sakibal623@gmail.com>
2022-09-23 23:53:06 +0600

    Deployment script of smart contract

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,13 +1,34 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  console.log(`Preparing deployment.....`);
+
   // Fetch contact to deploy
   const Token = await ethers.getContractFactory("Token");
+  const Exchange = await ethers.getContractFactory("Exchange");
+
+  // Fetch accounts
+  const accounts = await ethers.getSigners();
+  console.log(
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  );
 
   // Deploy contact
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log(`Token Deployed to: ${token.address}`);
+  const Sakib = await Token.deploy("Sakib", "SAK", "1000000");
+  await Sakib.deployed();
+  console.log(`SAK Deployed to: ${Sakib.address}`);
+
+  const Omar = await Token.deploy("Omar", "OM", "1000000");
+  await Omar.deployed();
+  console.log(`Omar Deployed to: ${Omar.address}`);
+
+  const Naga = await Token.deploy("Naga", "NG", "1000000");
+  await Naga.deployed();
+  console.log(`Naga Deployed to: ${Naga.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
 main().catch((error) => {
